f3b68602c9f77012b6b513cc2d67f51a3caeb4aa
rashida911 <rashirashi8389@gmail.com>
2023-03-02 15:08:26 +0530

    first commit

---
+++ Day3/src/assets/data.ts
@@ -0,0 +1,60 @@
+export const district = [
+    {
+        name: 'Alappuzha',
+        image: 'backwater alappy.jpg',
+        desc: ''
+    },
+    {
+        name: 'Ernakulam',
+        image: 'chinese fishing net.jpg'
+    },
+    {
+        name: 'Idukki',
+        image: 'meesappulimala.jpg'
+    },
+    {
+        name: 'Kannur',
+        image: 'Kavvayi islands.jpg'
+    },
+    {
+        name: 'Kasargod',
+        image: 'bekal-fort.jpg'
+    },
+    {
+        name: 'Kollam',
+        image: 'palaruvi falls.jpg'
+    },
+    {
+        name: 'Kottayam',
+        image: 'elaveezha.jpg'
+    },
+    {
+        name: 'Kozhikode',
+        image: 'kappad.jpg'
+    },
+    {
+        name: 'Malappuram',
+        image: 'kottakunnu.jpg'
+    },
+    {
+        name: 'Palakkad',
+        image: 'nelliyampathy.jpg'
+    },
+    {
+        name: 'Pathanamthitta',
+        image: 'gavi.jpg'
+    },
+    {
+        name: 'Thiruvananthapuram',
+        image: 'poovar.jpg'
+    },
+    {
+        name: 'Thrissur',
+        image: 'athirapally-falls.jpg'
+    },
+    {
+        name: 'Wayanad',
+        image: 'kuruva.jpg'
+    },
+
+]
